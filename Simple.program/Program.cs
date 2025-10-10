d598e4c160bb89198739c68c342e28b44fbd6dff
FridaNunez <fridamariano1012@gmail.com>
2025-10-10 17:25:56 -0400

    Adding my simple program

---
+++ Simple.program/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = 0;
+
+Console.WriteLine("Please insert a value: ");
+number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("The inserted value is even.");
+}
+else
+{
+    Console.WriteLine("The inserted value is uneven.");
+}
+Console.ReadKey();
