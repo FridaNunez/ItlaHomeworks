5f9f3787da546f15ee7f6863f6193a0d8b29f24f
FridaNunez <fridamariano1012@gmail.com>
2025-12-11 22:35:39 -0400

    Delete Model directory

--- Model/Categoria.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PlantStore.model
-{
-    public class Categoria
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = "";
-        public List<Planta> Plantas { get; set; } = new();
-    }
-}
