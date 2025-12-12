bb84eec27cc2354f757c7f5941606faa9aee3c34
FridaNunez <fridamariano1012@gmail.com>
2025-12-11 22:30:34 -0400

    Adding final project

---
+++ Final project/Model/PedidoPlanta.cs
@@ -0,0 +1,13 @@
+﻿namespace PlantStore.model
+{
+    public class PedidoPlanta
+    {
+        public int PedidoId { get; set; }
+        public Pedido? Pedido { get; set; }
+
+        public int PlantaId { get; set; }
+        public Planta? Planta { get; set; }
+
+        public int Cantidad { get; set; }
+    }
+}
